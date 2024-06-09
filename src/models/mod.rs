7925eebeedb058892f32a70aaa24a75fd91b4f67
ra890927 <happy.890927410@gmail.com>
2024-06-09 18:21:11 +0000

    feat: add submission

--- src/models/mod.rs
+++ src/models/mod.rs
@@ -8,3 +8,4 @@ use sea_orm::{DbErr, SqlErr};
 pub(crate) fn is_unique_constraint_violation_err(e: &DbErr) -> bool {
     matches!(e.sql_err(), Some(SqlErr::UniqueConstraintViolation(_)))
 }
+pub mod submissions;
---
+++ src/models/submissions.rs
@@ -0,0 +1,115 @@
+use loco_rs::prelude::*;
+use sea_orm::{Order, QueryOrder};
+use serde::Deserialize;
+
+use super::_entities::prelude::Submissions;
+pub use super::_entities::sea_orm_active_enums::{Language, SubmissionStatus};
+pub use super::_entities::submissions::{self, ActiveModel, Model};
+
+#[derive(Debug, Deserialize)]
+pub struct AddParams {
+    pub user: i32,
+    pub problem: i32,
+    pub timestamp: DateTime,
+    pub language: Language,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct ListParams {
+    pub offset: Option<usize>,
+    pub count: Option<usize>,
+    pub problem: Option<i32>,
+    pub user: Option<i32>,
+    pub status: Option<SubmissionStatus>,
+    pub language: Option<Language>,
+    pub course: Option<String>,
+}
+
+impl ActiveModelBehavior for ActiveModel {
+    // extend activemodel below (keep comment for generators)
+}
+
+impl ActiveModel {
+    pub async fn update_score<C: ConnectionTrait>(
+        mut self,
+        db: &C,
+        score: i32,
+    ) -> ModelResult<Model> {
+        self.score = ActiveValue::set(score);
+        Ok(self.update(db).await?)
+    }
+}
+
+impl Model {
+    /// Create a submission
+    ///
+    /// # Errors
+    ///
+    /// When could not save the problem into DB
+    pub async fn add<C: ConnectionTrait + TransactionTrait>(
+        db: &C,
+        params: &AddParams,
+    ) -> ModelResult<Self> {
+        let txn = db.begin().await?;
+
+        let submission = ActiveModel {
+            user_id: ActiveValue::set(params.user),
+            problem_id: ActiveValue::set(params.problem),
+            timestamp: ActiveValue::set(params.timestamp.clone()),
+            language: ActiveValue::set(params.language.clone()),
+            ..Default::default()
+        }
+        .insert(&txn)
+        .await?;
+
+        txn.commit().await?;
+
+        Ok(submission)
+    }
+
+    /// List submissions
+    ///
+    /// # Errors
+    ///
+    /// When cloud not query submissions from DB
+    pub async fn list<C: ConnectionTrait>(db: &C, params: &ListParams) -> ModelResult<Vec<Self>> {
+        let mut q = Submissions::find().order_by(submissions::Column::Id, Order::Asc);
+
+        if let Some(problem) = params.problem {
+            q = q.filter(submissions::Column::ProblemId.eq(problem));
+        }
+        if let Some(user) = params.user {
+            q = q.filter(submissions::Column::UserId.eq(user))
+        }
+        if let Some(status) = &params.status {
+            q = q.filter(submissions::Column::Status.eq(status.clone()))
+        }
+        if let Some(language) = &params.language {
+            q = q.filter(submissions::Column::Language.eq(language.clone()))
+        }
+
+        let submissions = q.all(db).await?.into_iter();
+
+        let offset = params.offset.unwrap_or(0);
+        let count = params.count.unwrap_or(usize::MAX);
+        let submissions = submissions.skip(offset).take(count);
+
+        Ok(submissions.collect())
+    }
+
+    pub async fn find_by_id<C: ConnectionTrait>(db: &C, id: i32) -> ModelResult<Self> {
+        Self::find_by_column(db, submissions::Column::Id, id).await
+    }
+
+    pub async fn find_by_column<C: ConnectionTrait>(
+        db: &C,
+        column: impl sea_orm::ColumnTrait,
+        value: impl Into<sea_orm::Value> + Send,
+    ) -> ModelResult<Self> {
+        let submission = submissions::Entity::find()
+            .filter(column.eq(value))
+            .one(db)
+            .await?;
+        submission.ok_or_else(|| ModelError::EntityNotFound)
+    }
+}
